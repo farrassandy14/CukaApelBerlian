2ad36e27b212e023398607dfe977963ce4c6eeb4
kulm2223@gmail.com <kulm2223@gmail.com>
2025-06-20 07:33:51 +0700

    Finish UI in HTML CSS JS

---
+++ core/api_const.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = "https://localhost/api/";
+
+// AUTH
+export const LOGIN_ENDPOINT = `${API_BASE_URL}auth/login`;
+export const REGISTER_ENDPOINT = `${API_BASE_URL}auth/register`;
